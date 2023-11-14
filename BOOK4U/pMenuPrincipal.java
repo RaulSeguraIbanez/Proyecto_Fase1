a55b4daa654fb9cba8a80058b1f3ff5b60c8236a
REGI120 <126282408+REGI120@users.noreply.github.com>
2023-11-14 15:58:18 +0100

    Update pMenuPrincipal.java

--- BOOK4U/pMenuPrincipal.java
+++ BOOK4U/pMenuPrincipal.java
@@ -15,113 +15,72 @@ public class pMenuPrincipal extends JFrame {
 	  private JButton inicioButton;
 	  private JButton irAPrincipalButton;
 	  
-    public pMenuPrincipal() {
-        setTitle("MENÚ PRINCIPAL");
-        setSize(800, 600);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	  public pMenuPrincipal() {
+	        // Configuración de la ventana principal
+	        setTitle("Menú Principal");
+	        setSize(600, 400);
+	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        setLocationRelativeTo(null); 
+	        setLayout(new BorderLayout());
+	        setBackground(new Color(255, 210, 175));
 
-        JPanel panel = new JPanel();
-        panel.setBackground(new Color(255, 210, 175));
-        panel.setLayout(new GridBagLayout());
+	        // Panel central con la imagen
+	        JPanel panelCentral = new JPanel();
+	        ImageIcon imagen = new ImageIcon("src/imagenes/LogoBOOK4U.png"); 
+	        JLabel labelImagen = new JLabel(imagen);
+	        panelCentral.add(labelImagen);
 
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(10, 10, 10, 10);
+	        // Panel inferior con los botones
+	        JPanel panelBotones = new JPanel();
+	        JButton btnVerReservas = new JButton("Ver Reservas");
+	        JButton btnIrAInicio = new JButton("Ir a Inicio");
+	        JButton btnIrAPrincipal = new JButton("Ir a Principal");
 
-        JPanel usuarioPanel = new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                // ... (unchanged)
-            }
-        };
+	       
+	        btnVerReservas.addActionListener(new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	               
+	                dispose(); 
+	                
+	                new pMisReservas().setVisible(true);
+	            }
+	        });
 
-        gbc.gridx = 1;
-        gbc.gridy = 1;
-        gbc.gridwidth = 4;
-        gbc.anchor = GridBagConstraints.CENTER;
-        usuarioPanel.setPreferredSize(new Dimension(200, 200));
-        panel.add(usuarioPanel, gbc);
+	        btnIrAInicio.addActionListener(new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	               
+	                dispose();
+	                new pInicio().setVisible(true);
+	            }
+	        });
 
-        ImageIcon empresaImage = new ImageIcon("src/imagenes/LogoBOOK4U.png");
-        JLabel empresaLabel = new JLabel(new ImageIcon(getRoundedImage(empresaImage.getImage(), 150, 150)));
-        gbc.gridy = 1;
-        panel.add(empresaLabel, gbc);
+	        btnIrAPrincipal.addActionListener(new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	              
+	                dispose();
+	                new pPrincipal().setVisible(true);
+	            }
+	        });
 
-        gbc.gridwidth = 1;
-        gbc.gridy = 2;
+	        panelBotones.add(btnVerReservas);
+	        panelBotones.add(btnIrAInicio);
+	        panelBotones.add(btnIrAPrincipal);
 
-       
-        add(panel);
+	        
+	        add(panelCentral, BorderLayout.CENTER);
+	        add(panelBotones, BorderLayout.SOUTH);
+	    }
 
-        setVisible(true);
-    }
-
-    private void addButton(JPanel panel, String text, ActionListener actionListener) {
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(10, 10, 10, 10);
-        gbc.gridx = 0;
-        gbc.anchor = GridBagConstraints.CENTER;
-
-        JButton button = new JButton(text);
-        button.setBackground(new Color(0, 102, 204));
-        button.setOpaque(true);
-        button.setBorderPainted(false);
-        button.setPreferredSize(new Dimension(150, 30));
-        button.setFocusPainted(false);
-        button.setFont(new Font("Arial", Font.BOLD, 16));
-        button.setForeground(Color.WHITE);
-        button.setBorder(new RoundButtonBorder(15));
-
-        // Add ActionListener to the button
-        button.addActionListener(actionListener);
-        
-        addButton(panel, "PPrincipal", e -> {
-            pPrincipal Principal = new pPrincipal();
-            Principal.setVisible(true);
-            this.dispose();
-        });
-
-        addButton(panel, "pMisReservas", e -> {
-            pMisReservas MisReservas = new pMisReservas();
-            MisReservas.setVisible(true);
-            this.dispose();
-        });
-
-        addButton(panel, "pInicio", e -> {
-            pInicio Inicio = new pInicio();
-            Inicio.setVisible(true);
-            this.dispose();
-        });
-
-        panel.add(button, gbc);
-    }
-
-    private Image getRoundedImage(Image image, int width, int height) {
-		return image;
-        // ... (unchanged)
-    }
-
-    private void showMessage(String message) {
-        JOptionPane.showMessageDialog(this, message);
-    }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new pMenuPrincipal());
-    }
-}
-
-   class RoundButtonBorder extends AbstractBorder {
-    private int radius;
-
-    public RoundButtonBorder(int radius) {
-        this.radius = radius;
-    }
-
-    @Override
-    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
-        Graphics2D g2 = (Graphics2D) g.create();
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.setColor(Color.BLACK);
-        g2.draw(new RoundRectangle2D.Double(x, y, width - 1, height - 1, radius, radius));
-        g2.dispose();
-    }
-}
+	    public static void main(String[] args) {
+	       
+	        SwingUtilities.invokeLater(new Runnable() {
+	            @Override
+	            public void run() {
+	                new pMenuPrincipal().setVisible(true);
+	            }
+	        });
+	    }
+	}
