af8e427ee7013df5556b6c4531942f6daaa0e0ff
RaulSeguraIbanez <128618606+RaulSeguraIbanez@users.noreply.github.com>
2023-11-23 16:08:45 +0100

    funcionalidad

--- BOOK4U/pNuevaReserva.java
+++ BOOK4U/pNuevaReserva.java
@@ -79,6 +79,13 @@ public class pNuevaReserva {
         JButton perfilButton = new JButton();
         ImageIcon icon = new ImageIcon(pFunciones.fotoUsuario);
         Image image = icon.getImage().getScaledInstance(40, 40, Image.SCALE_SMOOTH);
+        perfilButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Abrir la ventana del perfil con un DNI específico
+                SwingUtilities.invokeLater(() -> new pPerfil("DNI_DEL_USUARIO"));
+            }
+        });
         icon = new ImageIcon(image);
         perfilButton.setIcon(icon);
 
